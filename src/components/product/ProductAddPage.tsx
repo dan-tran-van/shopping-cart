e88fbb8a57ee0c07c945622e1006848aea740fb2
Dan Tran <tranvandanmail3@gmail.com>
2024-09-24 07:56:40 +0700

    feat: add form validation when uploading a product

--- src/components/product/ProductAddPage.tsx
+++ src/components/product/ProductAddPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Action } from "../Action";
 import { ActionItem } from "../ActionItem";
 import { Main } from "../Main";
@@ -6,11 +7,32 @@ import { ProductForm } from "./ProductForm";
 import "./ProductForm.css";
 
 export function ProductAddPage() {
+  const [formSubmitStatus, setFormSubmitStatus] = useState<
+    "notSubmitting" | "pending" | "error" | "success"
+  >("notSubmitting");
+  const [errorMessage, setErrorMessage] = useState("");
   return (
     <>
       <Nav isDetail={true}></Nav>
       <Main>
-        <ProductForm />
+        {formSubmitStatus === "success" && (
+          <div className="success-message">Success</div>
+        )}
+
+        {formSubmitStatus === "error" && (
+          <div className="error-message">{errorMessage}</div>
+        )}
+        {formSubmitStatus === "pending" && (
+          <div className="pending">Uploading...</div>
+        )}
+        {formSubmitStatus === "notSubmitting" && (
+          <ProductForm
+            formSubmitStatus={formSubmitStatus}
+            setFormSubmitStatus={setFormSubmitStatus}
+            errorMessage={errorMessage}
+            setErrorMessage={setErrorMessage}
+          />
+        )}
       </Main>
       <Action>
         <ActionItem handleClick={handleConfirm}>Confirm</ActionItem>
--- src/components/product/ProductForm.tsx
+++ src/components/product/ProductForm.tsx
@@ -1,34 +1,133 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CategoryFilter } from "../home/CategoryFilter";
+import { useFormik } from "formik";
 import "./ProductForm.css";
+import * as yup from "yup";
+import { Category } from "../../types/category.type";
 
-export function ProductForm() {
+let validationSchema = yup.object().shape({
+  title: yup.string().required("Required"),
+  price: yup.number().required("Required").positive("Must be a positive value"),
+  description: yup.string().required("Required"),
+  image: yup.string().url().required("Required"),
+  category: yup.string().test({
+    name: "is-not-select",
+    skipAbsent: false,
+    test(value, ctx) {
+      if (value === "select") {
+        return ctx.createError({ message: "Must select a value" });
+      }
+      return true;
+    },
+  }),
+});
+
+export function ProductForm({
+  formSubmitStatus,
+  setFormSubmitStatus,
+  errorMessage,
+  setErrorMessage,
+}) {
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      price: 0,
+      description: "",
+      image: "",
+      category: "select",
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      try {
+        setFormSubmitStatus("pending");
+        const response = await fetch("https://fakestoreapi.com/products", {
+          method: "POST",
+          body: JSON.stringify(values),
+        });
+        const json = await response.json();
+        if (json) {
+          setFormSubmitStatus("success");
+        }
+      } catch (error) {
+        setFormSubmitStatus("error");
+        setErrorMessage(error);
+      }
+    },
+  });
+
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  const [categoryFetchStatus, setCategoryFetchStatus] = useState<
+    "pending" | "success" | "error"
+  >("pending");
+  const [categoryFetchErrorMessage, setCategoryFetchErrorMessage] =
+    useState("");
+  useEffect(() => {
+    async function fetchAllCategories() {
+      setCategoryFetchStatus("pending");
+      try {
+        const response = await fetch(
+          "https://fakestoreapi.com/products/categories"
+        );
+        const json = await response.json();
+        setCategories(json);
+        setCategoryFetchStatus("success");
+      } catch (error) {
+        setErrorMessage(error.name);
+        setCategoryFetchStatus("error");
+      }
+    }
+
+    fetchAllCategories();
+  }, []);
   const [category, setCategory] = useState("select");
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleAddProduct(e);
-      }}
-    >
-      <label htmlFor="title" aria-required>
-        Title
-      </label>
-      <input type="text" name="title" id="title" required={true} />
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="title">Title</label>
+      <input type="text" id="title" {...formik.getFieldProps("title")} />
+      {formik.touched.title && formik.errors.title ? (
+        <div className="error">{formik.errors.title}</div>
+      ) : null}
       <label htmlFor="price">Price</label>
-      <input type="number" name="price" id="price" required={true} />
+      <input type="number" id="price" {...formik.getFieldProps("price")} />
+      {formik.touched.price && formik.errors.price ? (
+        <div className="error">{formik.errors.price}</div>
+      ) : null}
       <label htmlFor="description">Description</label>
-      <textarea name="description" id="description" required={true}></textarea>
+      <textarea
+        id="description"
+        {...formik.getFieldProps("description")}
+      ></textarea>
+      {formik.touched.description && formik.errors.description ? (
+        <div className="error">{formik.errors.description}</div>
+      ) : null}
       <label htmlFor="image">Image</label>
-      <input
-        type="url"
-        name="image"
-        id="image"
-        placeholder="https://example.com"
-        required={true}
-      />
+      <input type="url" id="image" {...formik.getFieldProps("image")} />
+      {formik.touched.image && formik.errors.image ? (
+        <div className="error">{formik.errors.image}</div>
+      ) : null}
       <label htmlFor="category">Category</label>
-      <CategoryFilter defaultCategory="select" setCategory={setCategory} />
+      {categoryFetchStatus === "error" && (
+        <div className="error">{categoryFetchErrorMessage}</div>
+      )}
+      {categoryFetchStatus === "pending" && (
+        <select id="category" disabled>
+          <option value="select">Loading...</option>
+        </select>
+      )}
+      {categoryFetchStatus === "success" && (
+        <select id="category" {...formik.getFieldProps("category")}>
+          <option value="select">Select</option>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      )}
+      {formik.touched.category && formik.errors.category ? (
+        <div className="error">{formik.errors.category}</div>
+      ) : null}
     </form>
   );
 }
